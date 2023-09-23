bd36114d36136059ff2806f93a87b7ed87d355e6
John Talghader <john.talghader@gmail.com>
2023-09-23 17:13:10 -0500

    fix header

--- frontend/src/HeaderStuff/Header.js
+++ frontend/src/HeaderStuff/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
   const fatGrams = 0;
 
   return (
-    <header className="upload-header">
+    <header className={HeaderCSS["upload-header"]}>
       <h2 onClick={() => {navigate("/")}} className={HeaderCSS["cal-tracker-txt"]}>CAL TRACKER</h2>
 
       <div className={HeaderCSS["right-header"]}>
