3af909365e276ac55298153de41d4ae947ed74f1
John Talghader <john.talghader@gmail.com>
2023-09-23 15:56:05 -0500

    fix conflicts

--- frontend/src/body/addFoodModal.js
+++ frontend/src/body/addFoodModal.js
@@ -1,8 +1,58 @@
-const  AddFoodModal= ({type, amount}) => {
+import { AiFillCaretLeft} from "react-icons/ai";
+
+const  AddFoodModal= ({isModal, setIsModal}) => {
+
+    function updateFood () {
+
+    }
+
+
     return (
+        <>
+
+        <div className="exit">
+            <AiFillCaretLeft className="exitIcon" color="white" size={30} onClick={() =>setIsModal(!isModal)}/>
+
+        </div>
+        
       <div className="addFoodModal">
-        <p> Add food</p>
+
+
+            <div className="addFoodManual">
+                <div className="addFoodImageContainer">
+                    <img className="saladImageFr" src="./images/salad.jpg" onClick={() => updateFood()}/>
+                </div>
+
+                <div className="addFoodManualTextDiv">
+                    <div className="addFoodManualTextDiv2">
+                        <p className="addFoodManualText">
+                        Add Manually
+                        </p>
+                    </div>
+                </div>
+            
+            </div>
+            <div className="addFoodUpload">
+                <div className="addFoodImageContainer">
+                    <img className="saladImageFr" src="./images/fruits.jpg" onClick={() => updateFood()}/>
+                </div>
+
+                <div className="addFoodUploadTextDiv">
+                    <div className="addFoodManualTextDiv2">
+                        <p className="addFoodManualText">
+                        Upload
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+
+
+        
+    
+
       </div>
+      </>
     );
   }
    
