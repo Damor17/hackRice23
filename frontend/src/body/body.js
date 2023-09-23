17d97ec729dadc8cb6c6683d67cb08ca3e5ce614
Carlos <cardanmoralesfr@gmail.com>
2023-09-23 16:59:01 -0500

    figuring out

--- frontend/src/body/body.js
+++ frontend/src/body/body.js
@@ -1,10 +1,16 @@
 import CategoryCard from "./categoryCard";
 import { useState } from "react";
 import AddFoodModal from "./addFoodModal";
+import ManualAdd from "./manualAdd";
+import UploadAdd from "./uploadAdd";
 
 const Body = ({data, setData}) => {
 
   const [isModal, setIsModal] = useState(false)
+
+  const [isManual, setIsManual] = useState(false)
+
+  const [isUpload, setIsUpload] = useState(false)
 
 
 
@@ -13,15 +19,24 @@ const Body = ({data, setData}) => {
     return (
       <>
         {isModal ? (
-          <AddFoodModal isModal={isModal} setIsModal= {setIsModal} onClick={() =>setIsModal(!isModal)}/>
-        ): (
+          <AddFoodModal isModal={isModal} setIsModal= {setIsModal} isManual={isManual} setIsManual= {setIsManual} isUpload= {isUpload} setIsUpload= {setIsUpload} />
+        ): isManual ? (
+          <ManualAdd isManual={isManual} setIsManual= {setIsManual}/>
+          
+
+        ): isUpload ? (
+          <UploadAdd isUpload= {isUpload} setIsUpload= {setIsUpload} />
+
+        ) : (
           <div className="divbody">
               {categories.map((item, index) => (
                 <CategoryCard key={index} item={item} data={data} setData={setData} isModal={isModal} setIsModal= {setIsModal} />
                 ))}
           </div>
 
-        )}
+        )
+        
+        }
       </>
     
     );
