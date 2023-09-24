bee7793b30dfaaf440df81bf9aa564a2894bef36
Anjali Yamasani <yamasani.apps@gmail.com>
2023-09-24 00:38:54 -0500

    upload page update

--- frontend/src/pages/Upload.js
+++ frontend/src/pages/Upload.js
@@ -1,6 +1,22 @@
+import { useState } from "react";
+import {AiFillCaretDown} from 'react-icons/ai'
+import UploadCSS from "../UploadStuff/Upload.module.css"
+
 const Upload = () => {
+  const [file, setFile] = useState()
+
+  function handleChange(e) {
+      setFile(URL.createObjectURL(e.target.files[0]));
+  }
+  
   return (
-    <h1>Upload Page</h1>
+    <div className={UploadCSS["UploadAdd"]}>
+      <input type="file" onChange={handleChange} />
+      <div className={UploadCSS["UploadImgContainer"]}>
+        {!file && <h2 className={UploadCSS["UploadHeaderText"]}>Upload Your Plate or a Nutrition Label</h2>}
+        {file && <img className={UploadCSS["UploadImage"]} src={file} />}
+      </div>
+    </div>
   );
 }
  
